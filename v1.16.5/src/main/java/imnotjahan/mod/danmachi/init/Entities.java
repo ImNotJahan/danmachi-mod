afda24b781ac649f197c150710bbbcf3fe2a0b72
ImNotJahan <hiimjahan@gmail.com>
2021-09-09 17:27:53 -0700

    Got spawn eggs working

--- v1.16.5/src/main/java/imnotjahan/mod/danmachi/init/Entities.java
+++ v1.16.5/src/main/java/imnotjahan/mod/danmachi/init/Entities.java
@@ -45,37 +45,37 @@ public class Entities
 
     //Monsters
     public static final RegistryObject<EntityType<Goblin>> GOBLIN = createEntity("goblin", Goblin::new,
-            1F, 1.7F);
+            1F, 1.7F, 32806, 9240576);
     public static final RegistryObject<EntityType<Minotaur>> MINOTAUR = createEntity("minotaur", Minotaur::new,
-            1F, 2.2F);
+            1F, 2.2F, 6968398, 14111828);
     public static final RegistryObject<EntityType<Almiraj>> ALMIRAJ = createEntity("almiraj", Almiraj::new,
-            0.5F, 0.5F);
+            0.5F, 0.5F, 11796480, 16777215);
     public static final RegistryObject<EntityType<MetalRabbit>> METAL_RABBIT = createEntity("metal_rabbit", MetalRabbit::new,
-            0.5F, 0.5F);
+            0.5F, 0.5F, 11184557, 11796480);
     public static final RegistryObject<EntityType<Bugbear>> BUGBEAR = createEntity("bugbear", Bugbear::new,
-            1, 1);
+            1, 1, 726570, 13121539);
 
     //Gods
     public static final RegistryObject<EntityType<Hestia>> HESTIA = createEntity("hestia", Hestia::new,
-            1F, 2F);
+            1F, 2F, 15527148, 4609420);
     public static final RegistryObject<EntityType<Hermes>> HERMES = createEntity("hermes", Hermes::new,
-            1F, 2F);
+            1F, 2F, 13087862, 2773329);
     public static final RegistryObject<EntityType<Soma>> SOMA = createEntity("soma", Soma::new,
-            1F, 2F);
+            1F, 2F, 6256533, 3947572);
     public static final RegistryObject<EntityType<Loki>> LOKI = createEntity("loki", Loki::new,
-            1F, 2F);
+            1F, 2F, 2105895, 5530296);
 
     //Other
     public static final RegistryObject<EntityType<GuildMember>> GUILD_MEMBER = createEntity("guild", GuildMember::new,
-            1, 2);
+            1, 2, 9849600, 16766720);
 
     private static <T extends Entity> RegistryObject<EntityType<T>>
-    createEntity(String name, EntityType.IFactory<T> factory, float width, float height)
+    createEntity(String name, EntityType.IFactory<T> factory, float width, float height, int primaryEgg, int secondaryEgg)
     {
         ResourceLocation location = new ResourceLocation(Reference.MODID, name);
         EntityType<T> entity = EntityType.Builder.of(factory, EntityClassification.MONSTER).sized(width, height)
                 .setTrackingRange(64).setUpdateInterval(1).build(location.toString());
-        Item spawnEgg = new SpawnEggItem(entity, 0, 16777215, (new Item.Properties()).tab(Main.EggGroup));
+        Item spawnEgg = new SpawnEggItem(entity, primaryEgg, secondaryEgg, (new Item.Properties()).tab(Main.EggGroup));
         spawnEgg.setRegistryName(new ResourceLocation(Reference.MODID, name + "_spawn_egg"));
         SPAWN_EGGS.add(spawnEgg);
 
