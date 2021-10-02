d1a84892ca10d9d1827024482e945356824796d9
ImNotJahan <hiimjahan@gmail.com>
2021-10-02 07:13:51 -0700

    Fixed capability provider

--- v1.16.5/src/main/java/imnotjahan/mod/danmachi/capabilities/StatusProvider.java
+++ v1.16.5/src/main/java/imnotjahan/mod/danmachi/capabilities/StatusProvider.java
@@ -1,5 +1,6 @@
 package imnotjahan.mod.danmachi.capabilities;
 
+import imnotjahan.mod.danmachi.util.exceptions.MissingStatus;
 import net.minecraft.nbt.INBT;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
@@ -7,15 +8,16 @@ import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 
+import javax.annotation.Nonnull;
+
 public class StatusProvider implements ICapabilitySerializable<INBT>
 {
     @CapabilityInject(IStatus.class)
     public static final Capability<IStatus> STATUS_CAP = null;
-    private static final LazyOptional<IStatus> lazyStatus = LazyOptional.of(Status::new);
-
-    private IStatus instance = STATUS_CAP.getDefaultInstance();
+    private final LazyOptional<IStatus> lazyStatus = LazyOptional.of(Status::new);
 
     @Override
+    @Nonnull
     public <T> LazyOptional<T> getCapability(Capability<T> capability, Direction side)
     {
         return capability == STATUS_CAP ? lazyStatus.cast() : LazyOptional.empty();
@@ -24,12 +26,12 @@ public class StatusProvider implements ICapabilitySerializable<INBT>
     @Override
     public INBT serializeNBT()
     {
-        return STATUS_CAP.getStorage().writeNBT(STATUS_CAP, this.instance, Status.capSide);
+        return STATUS_CAP.getStorage().writeNBT(STATUS_CAP, this.lazyStatus.orElseThrow(MissingStatus::new), Status.capSide);
     }
 
     @Override
     public void deserializeNBT(INBT nbt)
     {
-        STATUS_CAP.getStorage().readNBT(STATUS_CAP, this.instance, Status.capSide, nbt);
+        STATUS_CAP.getStorage().readNBT(STATUS_CAP, this.lazyStatus.orElseThrow(MissingStatus::new), Status.capSide, nbt);
     }
 }
